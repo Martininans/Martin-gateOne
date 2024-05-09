6859370ef8a3b6d3ea3450a2545d4a86db5413f8
Martin Inalegwu <Samuelinalegwu3559@gmail.com>
2024-05-09 14:10:14 +0100

    assingment

---
+++ dayFour/python/tax1.py
@@ -0,0 +1,9 @@
+matrix = [['' for _ in range(3)] for _ in range(3)]
+
+        print("Enter characters (x or o):")
+        for counter in range(3):
+            for count in range(3):
+                matrix[counter][count] = input()[0]
+
+        for row in matrix:
+            print(' '.join(row))
