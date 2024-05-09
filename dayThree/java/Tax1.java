47625ddccc4c7d1a939358c01bbd39ae3f96fbd0
Martin Inalegwu <Samuelinalegwu3559@gmail.com>
2024-05-09 14:01:52 +0100

    java work

---
+++ dayThree/java/Tax1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tax1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] matrix = new char[3][3];
+
+       
+	System.out.println("Enter characters (x or o):");
+	for (int counter = 0; counter < 3; counter++) {
+	for (int count = 0; count < 3; count++) {
+	matrix[counter][count] = scanner.next().charAt(0);
+            
+        }
+}
+	for (int counter = 0; counter < 3; counter++) {
+	for (int count = 0; count < 3; count++) {
+	System.out.print(matrix[counter][count] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
