c409c303b7bd218d89abd01491ad84b4787b6f9e
Martin Inalegwu <Samuelinalegwu3559@gmail.com>
2024-05-10 13:15:25 +0100

    today

--- dayFive/Tax1.java
+++ dayFive/Tax1.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
-public class Tax1{
-	public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);	
-	System.out.print("Enter card number: ");
-	String cardNumber = scanner.nextLine();
+public class Tax1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);    
+        System.out.print("Enter card number: ");
+        String cardNumber = scanner.nextLine();
 
-	int CARDNUMBER = 16;
-	
-	if(cardNumber.length() == CARDNUMBER ){
-		System.out.print( valid card number);
-	}else{
-		System.out.print(invalid card number);		
-	}
+        int CARDNUMBER = 16;
+        
+        if (cardNumber.length() == CARDNUMBER) {
+            System.out.println("Valid card number");
+        } else {
+            System.out.println("Invalid card number");        
+        }
 
-	char firstNumber = cardNumber.charAt(0);
-	char four = 4;
-	char five = "5";
-	char six = "6";
-	
-	if (firstNumber != four || firstNumber != five ||firstNumber != six){
-	System.out.print(cardNumber, + "is invalid");
-	}else{
-	System.out.printf(cardNumber, +  "is valid");
-	}
-}
+        char firstNumber = cardNumber.charAt(0);
+
+        if (firstNumber != '4' && firstNumber != '5' && firstNumber != '6') {
+            System.out.println(cardNumber + " is invalid");
+        } else {
+            System.out.println(cardNumber + " is valid");
+        }
+    }
 }
